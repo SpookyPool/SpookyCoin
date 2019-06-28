6999eb2dab769ea538df07ddd3c18493a427c1ad
SpookyPool <51341995+SpookyPool@users.noreply.github.com>
2019-06-29 00:16:29 +0200

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -183,9 +183,9 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const uint64_t BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  100;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              =  42069;
-const int      RPC_DEFAULT_PORT                              =  6969;
-const int      SERVICE_DEFAULT_PORT                          =  1337;
+const int      P2P_DEFAULT_PORT                              =  11420;
+const int      RPC_DEFAULT_PORT                              =  11421;
+const int      SERVICE_DEFAULT_PORT                          =  11422;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
